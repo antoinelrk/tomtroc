3430f0766e18d1c9fb6b3bbb86950b9e8193746e
Antoine LRK <admin@antoinelrk.com>
2024-04-25 07:33:16 +0200

    Add docs.

--- app/Helpers/Log.php
+++ app/Helpers/Log.php
@@ -2,12 +2,22 @@
 
 namespace App\Helpers;
 
+use JetBrains\PhpStorm\NoReturn;
+
 class Log
 {
+    /**
+     * Return dump of data and die process.
+     *
+     * @param mixed $data
+     * @return void
+     */
+    #[NoReturn]
     public static function dd(mixed $data): void
     {
-        echo '<pre style="background-color: rgba(15, 15, 15, 1); color: white; padding: 8px; line-height: 1.5">';
+        echo '<pre style="border-radius: 3px; background-color: rgba(15, 15, 15, 1); color: white; padding: 8px; line-height: 1.5">';
             var_dump($data);
         echo '</pre>';
+        die();
     }
 }
