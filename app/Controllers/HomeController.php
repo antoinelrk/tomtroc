d9fbb6bc8395490d835f9137baf572e130a1e2cc
Antoine LRK <admin@antoinelrk.com>
2024-04-26 08:37:34 +0200

    Prepare view overhaul

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -3,12 +3,29 @@
 namespace App\Controllers;
 
 use App\Core\Controller;
+use App\Core\Facades\View;
+use App\Core\Response;
 
 class HomeController extends Controller
 {
-    public function index(): void
+    public function index(): View
     {
-        // TODO: WIP! Return View manager OR Http response manager (For api usage)
-        echo 'It\'s works!';
+        // TODO: WIP! Return ViewRenderer manager OR Http response manager (For api usage)
+
+        return View::render('pages.home');
+    }
+
+    /**
+     * TODO: WIP! API Testing
+     *
+     * @return void
+     */
+    public function users(): void
+    {
+        $data = [
+            'foo' => 'bar',
+        ];
+
+        Response::json($data, Response::HTTP_OK);
     }
 }
--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,3 +1,21 @@
 <?php
+
+use App\Core\Facades\View;
+use App\Core\ViewRenderer;
+use App\Core\Router;
+use App\Core\Dispatcher;
+use App\Controllers\HomeController;
+
 require_once __DIR__ . '/../vendor/autoload.php';
-require_once __DIR__ . '/../routes/default.php';
+
+$router = new Router();
+
+$router->addRoute('GET','/', [HomeController::class, 'index']);
+$router->addRoute('GET', '/test', [HomeController::class, 'test']);
+$router->addRoute('GET', '/users', [HomeController::class, 'users']);
+
+$dispatcher = new Dispatcher($router);
+
+View::init(new ViewRenderer());
+
+$dispatcher->dispatch();
