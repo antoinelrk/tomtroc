02a2ce0725b589d3b1bacc3798eefae197e3e4b0
Antoine LRK <admin@antoinelrk.com>
2024-05-29 07:17:15 +0200

    Cleaning controller

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -3,32 +3,14 @@
 namespace App\Controllers;
 
 use App\Core\Controller;
-use App\Core\Database;
 use App\Core\Facades\View;
-use App\Core\Response;
 
 class HomeController extends Controller
 {
     public function index(): View
     {
-        Database::debug();
-
         return View::layout('layouts.app')
             ->view('pages.home')
             ->render();
-    }
-
-    /**
-     * TODO: WIP! API Testing
-     *
-     * @return void
-     */
-    public function users(): void
-    {
-        $data = [
-            'foo' => 'bar',
-        ];
-
-        Response::json($data, Response::HTTP_OK);
     }
 }
