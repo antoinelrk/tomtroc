4eb1587d289e49b2a6a31bcee4444148499a16aa
Antoine LRK <admin@antoinelrk.com>
2024-05-29 07:15:50 +0200

    Clean routes file and add new routes.

--- bootstrap/routes.php
+++ bootstrap/routes.php
@@ -1,13 +1,22 @@
 <?php
 
-use App\Controllers\HomeController;
 use App\Core\Router;
+
+use App\Controllers\AuthController;
+use App\Controllers\HomeController;
+use App\Controllers\UserController;
 
 $router = new Router();
 
 $router->addRoute('GET','/', [HomeController::class, 'index']);
-$router->addRoute('GET', '/users', [HomeController::class, 'users']);
+$router->addRoute('GET', '/users', [UserController::class, 'index']);
 
-$router->addRoute('GET','/auth/login', [AuthController::class, 'login']);
+// Authentication
+$router->addRoute('GET', '/auth/register', [AuthController::class, 'registerForm']);
+$router->addRoute('GET', '/auth/login', [AuthController::class, 'loginForm']);
+
+$router->addRoute('POST', '/auth/register', [AuthController::class, 'register']);
+$router->addRoute('POST', '/auth/login', [AuthController::class, 'login']);
+$router->addRoute('POST', '/auth/logout', [AuthController::class, 'logout']);
 
 return $router;
