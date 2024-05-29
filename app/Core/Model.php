5845198124aba48a28b6114526bdf3fba9456639
Antoine LRK <admin@antoinelrk.com>
2024-05-29 07:16:55 +0200

    Add hidden attribute, comment methods.

--- app/Core/Model.php
+++ app/Core/Model.php
@@ -5,7 +5,11 @@ namespace App\Core;
 class Model
 {
     protected $table;
+
     protected string $primaryKey = 'id';
+
+    protected $hidden = [];
+
     protected \PDO $connection;
 
     public function __construct()
@@ -14,37 +18,86 @@ class Model
             ->getConnection();
     }
 
-    public function all(): false|array
+    /**
+     * Exclude hidden's columns.
+     * @param array $data
+     *
+     * @return array
+     */
+    protected function withoutHidden(array $data): array
     {
-        $stmt = $this->connection->prepare("SELECT * FROM {$this->table}");
-        $stmt->execute();
-
-        return $stmt->fetchAll();
-    }
-
-    public function find($id) {
-        $stmt = $this->connection->prepare("SELECT * FROM {$this->table} WHERE {$this->primaryKey} = :id");
-        $stmt->bindParam(':id', $id);
-        $stmt->execute();
-
-        return $stmt->fetch();
-    }
-
-    public function create(array $data) {
-        $keys = implode(',', array_keys($data));
-        $values = ':' . implode(',:', array_keys($data));
-        $stmt = $this->connection->prepare("INSERT INTO {$this->table} ($keys) VALUES ($values)");
-
-        foreach ($data as $key => $value) {
-            $stmt->bindValue(":$key", $value);
+        foreach ($this->hidden as $hiddenColumn) {
+            if (isset($data[$hiddenColumn])) {
+                unset($data[$hiddenColumn]);
+            }
         }
 
-        $stmt->execute();
+        return $data;
+    }
+
+    /**
+     * Fetch all data on specific table
+     *
+     * @return false|array
+     */
+    public function all(): false|array
+    {
+        $statement = $this->connection->prepare("SELECT * FROM {$this->table}");
+        $statement->execute();
+        $data = $statement->fetchAll();
+
+        return array_map(
+            [$this, 'withoutHidden'], $data
+        );
+    }
+
+    /**
+     * Fetch specific data on specific table
+     *
+     * @param $id
+     * @return false|array
+     */
+    public function find($id): false|array
+    {
+        $statement = $this->connection->prepare("SELECT * FROM {$this->table} WHERE {$this->primaryKey} = :id");
+        $statement->bindParam(':id', $id);
+        $statement->execute();
+
+        return $this->withoutHidden($statement->fetch());
+    }
+
+    /**
+     * Create new model type entry in database
+     * @param array $data
+     *
+     * @return false|array
+     */
+    public function create(array $data): false|array
+    {
+        $keys = implode(',', array_keys($data));
+        $values = ':' . implode(',:', array_keys($data));
+        $statement = $this->connection->prepare("INSERT INTO {$this->table} ($keys) VALUES ($values)");
+
+        foreach ($data as $key => $value) {
+            $statement->bindValue(":$key", $value);
+        }
+
+        $statement->execute();
 
         return $this->find($this->connection->lastInsertId());
     }
 
-    public function update($id, array $data) {
+    /**
+     * Update entry in database.
+     * TODO: Create validation request
+     *
+     * @param $id
+     * @param array $data
+     *
+     * @return false|array
+     */
+    public function update($id, array $data): false|array
+    {
         $set = [];
 
         foreach ($data as $key => $value) {
@@ -52,23 +105,30 @@ class Model
         }
 
         $set = implode(',', $set);
-        $stmt = $this->connection->prepare("UPDATE {$this->table} SET $set WHERE {$this->primaryKey} = :id");
-        $stmt->bindParam(':id', $id);
+        $statement = $this->connection->prepare("UPDATE {$this->table} SET $set WHERE {$this->primaryKey} = :id");
+        $statement->bindParam(':id', $id);
 
         foreach ($data as $key => $value) {
-            $stmt->bindValue(":$key", $value);
+            $statement->bindValue(":$key", $value);
         }
 
-        $stmt->execute();
+        $statement->execute();
 
         return $this->find($id);
     }
 
+    /**
+     * Delete specific entry.
+     *
+     * @param $id
+     *
+     * @return void
+     */
     public function delete($id): void
     {
-        $stmt = $this->connection->prepare("DELETE FROM {$this->table} WHERE {$this->primaryKey} = :id");
-        $stmt->bindParam(':id', $id);
+        $statement = $this->connection->prepare("DELETE FROM {$this->table} WHERE {$this->primaryKey} = :id");
+        $statement->bindParam(':id', $id);
 
-        $stmt->execute();
+        $statement->execute();
     }
 }
