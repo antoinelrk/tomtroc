881e4777bbd3788cdd6780d92fd0f5b33cecddae
Antoine LRK <55787169+antoinelrk@users.noreply.github.com>
2024-09-09 06:21:47 +0200

    Added design to Errors page

--- app/Views/errors/http-errors.php
+++ app/Views/errors/http-errors.php
@@ -1,4 +1,7 @@
 <main class="errors-wrapper">
-    <h1><?= $error['code'] ?></h1>
+     <!-- <?php var_dump($_SESSION); ?> -->
+    <h1
+        class="error-code"
+    ><?= $error['code'] ?></h1>
     <h2><?= $error['message'] ?></h2>
 </main>
