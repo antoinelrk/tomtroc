300732951a3cfbd356b87a3d88c79b3ef88bebb5
Antoine LRK <55787169+antoinelrk@users.noreply.github.com>
2024-10-25 11:51:59 +0200

    Fix: Avatar uploads

--- dump.sql
+++ dump.sql
@@ -2,14 +2,14 @@
 -- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Host: database
--- Generation Time: Oct 11, 2024 at 09:37 AM
+-- Host: antoinelrk_database
+-- Generation Time: Oct 25, 2024 at 09:19 AM
 -- Server version: 10.4.4-MariaDB-1:10.4.4+maria~bionic
--- PHP Version: 8.2.15
+-- PHP Version: 8.1.17
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
-SET time_zone = "+00:00";
+SET time_zone = "+02:00";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
@@ -28,16 +28,16 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `books` (
-  `id` int(11) NOT NULL,
-  `title` varchar(255) COLLATE latin1_general_ci NOT NULL,
-  `slug` varchar(255) COLLATE latin1_general_ci NOT NULL,
-  `author` varchar(255) COLLATE latin1_general_ci NOT NULL,
-  `cover` varchar(255) COLLATE latin1_general_ci DEFAULT NULL,
-  `description` longtext COLLATE latin1_general_ci NOT NULL,
-  `available` tinyint(1) NOT NULL DEFAULT 0,
-  `user_id` int(11) DEFAULT NULL,
-  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
-  `updated_at` timestamp NOT NULL DEFAULT current_timestamp()
+                         `id` int(11) UNSIGNED NOT NULL,
+                         `title` varchar(255) COLLATE latin1_general_ci NOT NULL,
+                         `slug` varchar(255) COLLATE latin1_general_ci NOT NULL,
+                         `author` varchar(255) COLLATE latin1_general_ci NOT NULL,
+                         `cover` varchar(255) COLLATE latin1_general_ci DEFAULT NULL,
+                         `description` longtext CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+                         `available` tinyint(1) NOT NULL DEFAULT 0,
+                         `user_id` int(11) DEFAULT NULL,
+                         `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+                         `updated_at` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
 
 -- --------------------------------------------------------
@@ -47,13 +47,13 @@ CREATE TABLE `books` (
 --
 
 CREATE TABLE `conversations` (
-  `id` bigint(20) UNSIGNED NOT NULL,
-  `receiver_id` bigint(20) UNSIGNED DEFAULT NULL,
-  `sender_id` bigint(20) UNSIGNED DEFAULT NULL,
-  `uuid` varchar(255) COLLATE latin1_general_ci NOT NULL,
-  `archived` tinyint(1) NOT NULL DEFAULT 0,
-  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
-  `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+                                 `id` int(11) UNSIGNED NOT NULL,
+                                 `receiver_id` bigint(20) NOT NULL,
+                                 `sender_id` int(11) NOT NULL,
+                                 `uuid` varchar(255) COLLATE latin1_general_ci NOT NULL,
+                                 `archived` tinyint(1) NOT NULL,
+                                 `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+                                 `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
 
 -- --------------------------------------------------------
@@ -63,11 +63,11 @@ CREATE TABLE `conversations` (
 --
 
 CREATE TABLE `conversation_user` (
-  `id` bigint(20) UNSIGNED NOT NULL,
-  `conversation_id` bigint(20) UNSIGNED DEFAULT NULL,
-  `user_id` bigint(20) UNSIGNED DEFAULT NULL,
-  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
-  `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+                                     `id` int(11) UNSIGNED NOT NULL,
+                                     `conversation_id` int(11) DEFAULT NULL,
+                                     `user_id` int(11) DEFAULT NULL,
+                                     `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+                                     `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
 
 -- --------------------------------------------------------
@@ -77,15 +77,15 @@ CREATE TABLE `conversation_user` (
 --
 
 CREATE TABLE `messages` (
-  `id` bigint(20) UNSIGNED NOT NULL,
-  `conversation_id` bigint(20) UNSIGNED DEFAULT NULL,
-  `parent_id` bigint(20) UNSIGNED DEFAULT NULL,
-  `sender_id` bigint(20) UNSIGNED DEFAULT NULL,
-  `receiver_id` bigint(20) UNSIGNED DEFAULT NULL,
-  `content` longtext COLLATE latin1_general_ci NOT NULL,
-  `readed` tinyint(1) NOT NULL DEFAULT 0,
-  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
-  `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+                            `id` int(11) UNSIGNED NOT NULL,
+                            `conversation_id` int(11) DEFAULT NULL,
+                            `parent_id` int(11) DEFAULT NULL,
+                            `content` longtext COLLATE latin1_general_ci NOT NULL,
+                            `readed` tinyint(1) NOT NULL DEFAULT 0,
+                            `sender_id` int(11) NOT NULL,
+                            `receiver_id` int(11) NOT NULL,
+                            `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+                            `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
 
 -- --------------------------------------------------------
@@ -95,24 +95,14 @@ CREATE TABLE `messages` (
 --
 
 CREATE TABLE `users` (
-  `id` bigint(20) NOT NULL,
-  `username` varchar(64) COLLATE latin1_general_ci NOT NULL,
-  `email` varchar(64) COLLATE latin1_general_ci NOT NULL,
-  `password` varchar(255) COLLATE latin1_general_ci NOT NULL,
-  `avatar` varchar(255) COLLATE latin1_general_ci DEFAULT NULL,
-  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
-  `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+                         `id` int(11) UNSIGNED NOT NULL,
+                         `username` varchar(64) COLLATE latin1_general_ci NOT NULL,
+                         `email` varchar(64) COLLATE latin1_general_ci NOT NULL,
+                         `password` varchar(255) COLLATE latin1_general_ci NOT NULL,
+                         `avatar` varchar(255) COLLATE latin1_general_ci DEFAULT NULL,
+                         `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+                         `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
-
---
--- Dumping data for table `users`
---
-
-INSERT INTO `users` (`id`, `username`, `email`, `password`, `avatar`, `created_at`, `updated_at`) VALUES
-(1, 'JohnDoe', 'john@doe.fr', '$argon2id$v=19$m=65536,t=4,p=1$cmY4N1Zsa2ZramdkUG8veA$k3j/R/2B53nYdd39dRxzpQGSGc2T8IkQOpEeleUKNW0', NULL, '2024-10-11 09:36:47', '2024-10-11 09:36:47'),
-(2, 'JaneDoe', 'jane@doe.fr', '$argon2id$v=19$m=65536,t=4,p=1$SzduV1lPSnU3VjM2aFluZQ$+ehVgPkKTfM5veY9LvWh13cglXMQIIHzL6qTnnTLcWs', NULL, '2024-10-11 09:36:58', '2024-10-11 09:36:58'),
-(3, 'JackDoe', 'jack@doe.fr', '$argon2id$v=19$m=65536,t=4,p=1$M1VkMDlFNVJRNElha0oyag$wrDJXMBkX5tmgfgtI/VeAN67NU7LxbCwqyhCofs4Lhw', NULL, '2024-10-11 09:37:15', '2024-10-11 09:37:15'),
-(4, 'JohannaDoe', 'johanna@doe.fr', '$argon2id$v=19$m=65536,t=4,p=1$S3MzTFNxd01DUWhHeldiUw$wiHi2FfhQm07kO2jex3EKP27YURpeUSqnxulRUxOcfI', NULL, '2024-10-11 09:37:35', '2024-10-11 09:37:35');
 
 --
 -- Indexes for dumped tables
@@ -122,33 +112,31 @@ INSERT INTO `users` (`id`, `username`, `email`, `password`, `avatar`, `created_a
 -- Indexes for table `books`
 --
 ALTER TABLE `books`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `slug` (`slug`) USING BTREE;
+    ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `conversations`
 --
 ALTER TABLE `conversations`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `uuid` (`uuid`);
+    ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `conversation_user`
 --
 ALTER TABLE `conversation_user`
-  ADD PRIMARY KEY (`id`);
+    ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `messages`
 --
 ALTER TABLE `messages`
-  ADD PRIMARY KEY (`id`);
+    ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`);
+    ADD PRIMARY KEY (`id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -158,31 +146,31 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `books`
 --
 ALTER TABLE `books`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+    MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `conversations`
 --
 ALTER TABLE `conversations`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+    MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `conversation_user`
 --
 ALTER TABLE `conversation_user`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+    MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `messages`
 --
 ALTER TABLE `messages`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+    MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+    MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
